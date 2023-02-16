9f986a531b38e4d24c2c3a8e6f088a80eeae8e30
Gid.Lamim <gid.lamim@gmail.com>
2023-02-16 20:42:52 -0300

    create player and game objects

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 window.addEventListener("load", function () {
   const canvas = document.getElementById("canvas1");
   const ctx = canvas.getContext("2d");
+
   //set canvas size
   canvas.width = 1280;
   canvas.height = 720;
+
+  //object player
+  class Player {}
+
+  //the class Game will handle all the game logics
+  class Game {}
 });
