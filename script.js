03326236461bce5c233b7f097840e4a6aea24d76
Gid.Lamim <gid.lamim@gmail.com>
2023-02-16 20:39:52 -0300

    set canvas size

---
+++ script.js
@@ -0,0 +1,8 @@
+//addEventListener('load' --> when the page loads
+window.addEventListener("load", function () {
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  //set canvas size
+  canvas.width = 1280;
+  canvas.height = 720;
+});
